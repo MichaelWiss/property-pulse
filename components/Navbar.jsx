dc59f2a23f5ec096c9d81899652936c504bab700
MichaelWiss <michael.wiss@gmail.com>
2024-08-02 15:45:54 -0500

    ProfileMenu state

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,6 +10,7 @@ import { FaGoogle } from 'react-icons/fa';
 
 const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
 
     return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -135,6 +136,7 @@ const Navbar = () => {
                     id="user-menu-button"
                     aria-expanded="false"
                     aria-haspopup="true"
+                    onClick={ () => setIsProfileMenuOpen((prev) => !prev) }
                   >
                     <span className="absolute -inset-1.5"></span>
                     <span className="sr-only">Open user menu</span>
@@ -147,9 +149,11 @@ const Navbar = () => {
                 </div>
   
                 {/* <!-- Profile dropdown --> */}
+                {
+                  isProfileMenuOpen && (
                 <div
                   id="user-menu"
-                  className="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                   role="menu"
                   aria-orientation="vertical"
                   aria-labelledby="user-menu-button"
@@ -171,15 +175,14 @@ const Navbar = () => {
                     id="user-menu-item-2"
                     >Saved Properties</Link
                   >
-                  <a
-                    href="#"
+                  <button
                     className="block px-4 py-2 text-sm text-gray-700"
                     role="menuitem"
                     tabIndex="-1"
                     id="user-menu-item-2"
-                    >Sign Out</a
-                  >
+                    >Sign Out</button>
                 </div>
+                  )}
               </div>
             </div>
           </div>
