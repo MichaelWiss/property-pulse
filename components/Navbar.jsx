ac136156f66b70c6dd5e3077222c5d0399ecc590
MichaelWiss <michael.wiss@gmail.com>
2024-08-05 16:59:55 -0500

    isLoggedIn

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,6 +12,7 @@ import { FaGoogle } from 'react-icons/fa';
 const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     const pathname = usePathname();
 
@@ -73,19 +74,23 @@ const Navbar = () => {
                     </Link>
                   <Link
                     href="/properties"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                    className={`${pathname === '/properties' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}>
                       Properties
                     </Link>
+                    {isLoggedIn && (
                   <Link
                     href="/properties/add"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                    className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}>
                       Add Property
                     </Link>
+                  )}
                 </div>
               </div>
             </div>
   
             {/* <!-- Right Side Menu (Logged Out) --> */}
+
+            {!isLoggedIn && (
             <div className="hidden md:block md:ml-6">
               <div className="flex items-center">
                 <button
@@ -96,8 +101,11 @@ const Navbar = () => {
                 </button>
               </div>
             </div>
-  
+          )}
+
             {/* <!-- Right Side Menu (Logged In) --> */}
+            {
+              isLoggedIn && (
             <div
               className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0"
             >
@@ -188,9 +196,10 @@ const Navbar = () => {
                   )}
               </div>
             </div>
+              )}
           </div>
         </div>
-  
+        
         {/* <!-- Mobile menu, show/hide based on menu state. --> */}
         {
           isMobileMenuOpen && (
@@ -198,25 +207,29 @@ const Navbar = () => {
           <div className="space-y-1 px-2 pb-3 pt-2">
             <Link
               href="/"
-              className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium">
+              className={`${pathname === '/' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}>
                 Home
               </Link>
             <Link
               href="/properties"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-              >Properties</Link
+              className={`${pathname === '/properties' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}>Properties</Link
             >
+            { isLoggedIn && (
             <Link
               href="properties/add"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-              >Add Property</Link
+              className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}>Add Property</Link
             >
+            )}
+            {
+              !isLoggedIn && (
             <button
               className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4"
             >
               <i className="fa-brands fa-google mr-2"></i>
               <span>Login or Register</span>
             </button>
+          )
+        }
           </div>
         </div>
       )}
