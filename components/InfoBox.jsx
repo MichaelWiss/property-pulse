514387741391778b3ad795e831dc8905f1e54717
MichaelWiss <michael.wiss@gmail.com>
2024-08-07 20:33:13 -0500

    heading prop

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -1,7 +1,7 @@
 const InfoBox = ({ heading, children }) => {
     return (
         <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-              <h2 className="text-2xl font-bold">For Renters</h2>
+              <h2 className="text-2xl font-bold">{ heading }</h2>
               <p className="mt-2 mb-4">
                 {children}
               </p>
--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -5,8 +5,8 @@ const InfoBoxes = () => {
         <section>
         <div className="container-xl lg:container m-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-            <InfoBox>Find your dream rental property. Bookmark properties and contact owners.</InfoBox>
-            <InfoBox>List your properties and reach potential tenants. Rent as an airbnb or long term.</InfoBox>
+            <InfoBox heading="For Renters">Find your dream rental property. Bookmark properties and contact owners.</InfoBox>
+            <InfoBox heading="For Property Owners">List your properties and reach potential tenants. Rent as an airbnb or long term.</InfoBox>
 
           </div>
         </div>
