7d58afa297ef417b431377c87acc359380d0bbaf
MichaelWiss <michael.wiss@gmail.com>
2024-08-14 19:17:29 -0500

    mongoDB connection file

--- config/database.js
+++ config/database.js
@@ -3,5 +3,20 @@ import mongoose from "mongoose";
 let connected = false;
 
 const connectDB = async () => {
-    mongoose.set()
-}
+    mongoose.set('strictQuery', true);
+
+    // If the database is already connected, don't connect again
+    if(connected) {
+        console.log('MongoDB is already connected');
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        connected = true;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default connectDB;
