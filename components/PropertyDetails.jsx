de4d845ee5f93a0e330b26bd1b3719f44f54c954
MichaelWiss <michael.wiss@gmail.com>
2024-08-20 17:16:34 -0500

    PropertyDetails

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -1,10 +1,12 @@
+import { FaTimes } from "react-icons/fa";
+
 const PropertyDetails = ({ property }) => {
     return (  <main>
         <div
           className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
         >
-          <div className="text-gray-500 mb-4">Apartment</div>
-          <h1 className="text-3xl font-bold mb-4">Boston Commons Retreat</h1>
+          <div className="text-gray-500 mb-4">{property.type}</div>
+          <h1 className="text-3xl font-bold mb-4">{property.name}</h1>
           <div
             className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
           >
@@ -12,7 +14,7 @@ const PropertyDetails = ({ property }) => {
               className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
             ></i>
             <p className="text-orange-700">
-              120 Tremont Street Boston, MA 02111
+              {property.location.street}, {property.location.city} {property.location.zipcode}
             </p>
           </div>
 
@@ -24,19 +26,35 @@ const PropertyDetails = ({ property }) => {
               className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0"
             >
               <div className="text-gray-500 mr-2 font-bold">Nightly</div>
-              <div className="text-2xl font-bold">
-                <i className="fa fa-xmark text-red-700"></i>
+              <div className="text-2xl font-bold text-blue-500">
+                { property.rates.nightly ? (
+                    `$${property.rates.nightly.toLocaleString()}`
+                ) : (
+                    <FaTimes className="text-red-700"/>
+                )}
               </div>
             </div>
             <div
               className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0"
             >
               <div className="text-gray-500 mr-2 font-bold">Weekly</div>
-              <div className="text-2xl font-bold text-blue-500">$1,100</div>
+              <div className="text-2xl font-bold text-blue-500">
+              { property.rates.weekly ? (
+                    `$${property.rates.weekly.toLocaleString()}`
+                ) : (
+                    <FaTimes className="text-red-700"/>
+                )}
+              </div>
             </div>
             <div className="flex items-center justify-center mb-4 pb-4 md:pb-0">
               <div className="text-gray-500 mr-2 font-bold">Monthly</div>
-              <div className="text-2xl font-bold text-blue-500">$4,200</div>
+              <div className="text-2xl font-bold text-blue-500">
+              { property.rates.monthly ? (
+                    `$${property.rates.monthly.toLocaleString()}`
+                ) : (
+                    <FaTimes className="text-red-700"/>
+                )}
+              </div>
             </div>
           </div>
         </div>
@@ -48,7 +66,7 @@ const PropertyDetails = ({ property }) => {
           >
             <p>
               <i className="fa-solid fa-bed"></i> 3
-              <span className="hidden sm:inline">Beds</span>
+              <span className="hidden sm:inline">{property.beds}</span>
             </p>
             <p>
               <i className="fa-solid fa-bath"></i> 2
