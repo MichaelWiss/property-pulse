e2e42763421d6293da277dadcad85bd0b8b076f6
MichaelWiss <michael.wiss@gmail.com>
2024-08-22 17:49:23 -0500

    signIn

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -107,12 +107,15 @@ const Navbar = () => {
             {!session && (
             <div className="hidden md:block md:ml-6">
               <div className="flex items-center">
-                <button
+              { providers && Object.values(providers).map((provider, index) => (
+                <button key={index}
+                onClick={ () => signIn(provider.id) }
                   className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   <FaGoogle className='text-white mr-2'/>
                   <span>Login or Register</span>
                 </button>
+              )) }
               </div>
             </div>
           )}
