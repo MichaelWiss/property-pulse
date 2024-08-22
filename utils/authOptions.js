1907680c267033b0acd1fe87bd12bb7ddf5ecc6a
MichaelWiss <michael.wiss@gmail.com>
2024-08-21 20:00:41 -0500

    route.js

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -17,7 +17,16 @@ export const authOptions = {
     callbacks: {
         //Invoked on successful sign in 
         async signIn({ profile }) {
-            
-        }
+            // 1. Connect to the database
+            // 2. Check if user exists
+            // 3.  If not, create user
+            // 4. Return true to allow sign in
+        },
+        // Session callback function that modifies the session object
+        async session ({ session }) { 
+            // 1. Get the use from database
+            // 2. Assign user id from the session
+            // 3. Return session
+        },
     }
 };
