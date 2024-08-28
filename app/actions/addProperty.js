daf6ec2d75e7103092799e852774ee455e6237cd
MichaelWiss <michael.wiss@gmail.com>
2024-08-27 20:35:39 -0500

    amenities bug

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,7 +1,7 @@
 'use server';
 
 async function addProperty(formData) {
-    console.log(formData.getAll('amenities'));
+    const amenities = formData.getAll('amenities');
     const images = formData
     .getAll('images')
     .filter((image) => image.name !== '')
@@ -18,10 +18,22 @@ async function addProperty(formData) {
             zipcode: formData.get('location.zipcode'),
         },
         beds: formData.get('beds'),
-        name: formData.get('name'),
-        name: formData.get('name'),
-
+        baths: formData.get('baths'),
+        square_feet: formData.get('square_feet'),
+        amenities,
+        rates: {
+           nightly: formData.get('rates.nightly'),
+           weekly: formData.get('rates.weekly'),
+           monthly: formData.get('rates.monthly'),
+        },
+        seller_info: {
+            name: formData.get('seller_info.name'),
+            email: formData.get('seller_info.email'),
+            phone: formData.get('seller_info.phone'),
+        },
+        images,
     };
+    console.log(propertyData);
 }
 
 export default addProperty;
