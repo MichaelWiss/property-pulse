8dc5a81ba740a16f34e6c8b92e319042108d7307
MichaelWiss <michael.wiss@gmail.com>
2024-08-27 19:45:23 -0500

    addProperty form

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -2,6 +2,26 @@
 
 async function addProperty(formData) {
     console.log(formData.getAll('amenities'));
+    const images = formData
+    .getAll('images')
+    .filter((image) => image.name !== '')
+    .map((image) => image.name);
+    
+    const propertyData = {
+        type: formData.get('type'),
+        name: formData.get('name'),
+        description: formData.get('description'),
+        location: {
+            street: formData.get('location.street'),
+            city: formData.get('location.city'),
+            state: formData.get('location.state'),
+            zipcode: formData.get('location.zipcode'),
+        },
+        beds: formData.get('beds'),
+        name: formData.get('name'),
+        name: formData.get('name'),
+
+    };
 }
 
 export default addProperty;
