90e8d5d5b5e8cd6e3e13757467dfd8e2254bf7b4
MichaelWiss <michael.wiss@gmail.com>
2024-08-30 12:21:28 -0500

    PropertyImages

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,6 @@
 import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import PropertyDetails from '@/components/PropertyDetails';
+import PropertyImages from '@/components/PropertyImages';
 import connectDB from "@/config/database"; 
 import Property from "@/models/Property";
 import Link from 'next/link';
@@ -32,6 +33,7 @@ const PropertyPage = async ({ params }) => {
                         </div>
                     </div>
                 </section>
+                <PropertyImages />
          </>
     );
 };
