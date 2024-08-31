0c891b3d6d8df8053c161f14e5d71db04af7c277
MichaelWiss <michael.wiss@gmail.com>
2024-08-31 12:13:58 -0500

    conditional css

--- components/PropertyImages.jsx
+++ components/PropertyImages.jsx
@@ -17,7 +17,11 @@ const PropertyImages = ({ images }) => {
                 ) : (
                 <div className="grid grid-cols-2 gap-4">
                     { images.map((image, index) => (
-                        <div key={index} className='cols-span-1'>
+                        <div key={index} className={ `${images.length === 3 && index === 2
+                            ? 'col-span-2' 
+                            : 'col-span-1'
+                         }`}
+                         >
                         <Image 
                             src={image}
                             alt=''
