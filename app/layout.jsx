f3d252ee0796237d5d6a4f0a49601101ad8c3999
MichaelWiss <michael.wiss@gmail.com>
2024-09-06 18:09:35 -0500

    toast message

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import AuthProvider from '@/components/AuthProvider';
-import { ToastContainer } from 'react-toastify.css';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '@/assets/styles/globals.css';
 
