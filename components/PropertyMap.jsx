379df5057e0e9ad8b5b5fc30f2301e12ef2aea1e
MichaelWiss <michael.wiss@gmail.com>
2024-09-10 19:08:08 -0500

    fetchCoords

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -13,6 +13,29 @@ const PropertyMap = ({ property }) => {
         height: '500px'
     });  
     const [loading, setLoading] = useState(true);
+    const [geoCodeError, setGeoCodeError] = useState(false);
+
+    setDefaults({
+        key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
+        language: 'en',
+        region: 'us'
+    });
+
+    useEffect(() => {
+        const fetchCoords = async () => {
+            try {
+                const res = await fromAddress(`${property.location.street} ${property.location.city} ${property.location.state} ${property.location.zipcode}`);
+                // check geocode results
+            } catch (error) {
+                console.log(error);
+                setGeoCodeError(true);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchCoords();
+    },[]);
 
     return ( <div>Map</div> );
 }
