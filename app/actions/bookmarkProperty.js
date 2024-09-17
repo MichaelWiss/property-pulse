14903d43a26172045b5ac083628904aacbef2dc6
MichaelWiss <michael.wiss@gmail.com>
2024-09-17 13:48:02 -0500

    bookmark logic

--- app/actions/bookmarkProperty.js
+++ app/actions/bookmarkProperty.js
@@ -15,11 +15,16 @@ async function bookmarkProperty(propertyId) {
 
  const { userId } = sessionUser;
 
+ const user = await User.findById(userId);
+
+ const isBookmarked = user.bookmarks.includes(propertyId);
+
  let message;
 
- 
-
-
+ if(isBookmarked) {
+   // if already bookmarked, then remove
+   
+ }
 }
 
 export default bookmarkProperty;
