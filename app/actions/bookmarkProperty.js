88c52098099d4a07dddec257c4835782f92ae6fe
MichaelWiss <michael.wiss@gmail.com>
2024-09-17 19:22:29 -0500

    isBookmarked

--- app/actions/bookmarkProperty.js
+++ app/actions/bookmarkProperty.js
@@ -17,7 +17,7 @@ async function bookmarkProperty(propertyId) {
 
  const user = await User.findById(userId);
 
- const isBookmarked = user.bookmarks.includes(propertyId);
+ let isBookmarked = user.bookmarks.includes(propertyId);
 
  let message;
 
@@ -25,8 +25,15 @@ async function bookmarkProperty(propertyId) {
    // if already bookmarked, then remove
    user.bookmarks.pull(propertyId);
    message = 'Bookmark Removed';
-   isBookmarked = true;
+   isBookmarked = false;
+ } else {
+  // if not bookmarked, then add
+  user.bookmarks.push(propertyId);
+  message = 'Bookmark Added';
+  isBookmarked = true;
  }
+
+ 
  await user.save();
  revalidatePath('/properties/saved', 'page');
 
