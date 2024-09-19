4c9d8a9d751662d558f4723e688ca2190097b6b2
MichaelWiss <michael.wiss@gmail.com>
2024-09-19 17:04:50 -0500

    convertToSerializableObject

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,7 +4,7 @@ import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
 import profileDefault from '@/assets/images/profile.png';
 import ProfileProperties from "@/components/ProfileProperties";
-import { convertToSerializableObject } from "@/utils/convertToObject";
+import { convertToSerializeableObject } from "@/utils/convertToObject";
 
 
 const ProfilePage = async () => {
@@ -20,7 +20,7 @@ const ProfilePage = async () => {
 
     const propertiesDocs = await Property.find({ owner: userId }).lean();
    
-    const properties = propertiesDocs.map(convertToSerializableObject);
+    const properties = propertiesDocs.map(convertToSerializeableObject);
 
 
     return (
