86b97e5fca6888e18579e6de179fd3594064f663
MichaelWiss <michael.wiss@gmail.com>
2024-09-25 18:42:16 -0500

    ...Loading

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -38,6 +38,10 @@ const  BookmarkButton = ({ property }) => {
         });
     };
 
+    if (loading) {
+      return <p className='text-center'>Loading...</p>
+    }
+
     return isBookmarked ? (
       <button
       className='bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'
