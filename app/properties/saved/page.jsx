03198049f753e9b9a02dee2bb3f835df6872c901
MichaelWiss <michael.wiss@gmail.com>
2024-09-25 19:00:53 -0500

    saved

---
+++ app/properties/saved/page.jsx
@@ -0,0 +1,16 @@
+import { getSessionUser } from "@/utils/getSessionUser";
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+const SavedPropertiesPage = async () => {
+    const { userId } = await getSessionUser();
+
+    const { bookmarks } = await User.findById(userId).populate('bookmarks');
+     
+    console.log(bookmarks);
+
+    return <div>Saved</div>;
+}
+ 
+export default SavedPropertiesPage;
