e823f9b9aa7ec956eddbb597743aa493cfed1382
MichaelWiss <michael.wiss@gmail.com>
2024-09-26 12:08:07 -0500

    mapping saved properties

--- app/properties/saved/page.jsx
+++ app/properties/saved/page.jsx
@@ -11,9 +11,20 @@ const SavedPropertiesPage = async () => {
     const { bookmarks } = await User.findById(userId)
         .populate('bookmarks');
      
-    console.log(bookmarks);
+  
 
-    return <div>Saved</div>;
-}
+    return <section className='px-4 py-6'>
+        <div className="container lg:container m-auto px-4 py-6">
+            <h1 className="text-2xl mb-4">Saved Properties</h1>
+            { bookmarks.length ===0 ? (<p>No Saved Properties</p>) : (
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    {bookmarks.map((property) => (
+                        <PropertyCard key={property._id} property={property} />
+                    ))}
+                </div>
+            ) }
+        </div>
+    </section>;
+};
  
 export default SavedPropertiesPage;
