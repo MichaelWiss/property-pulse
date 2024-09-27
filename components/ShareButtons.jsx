acb01622106f404afb6d6365181d2ba6efeb9f19
MichaelWiss <michael.wiss@gmail.com>
2024-09-27 16:15:15 -0500

    shareUrl

--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -1,6 +1,19 @@
-import { FaShare } from "react-icons/fa";
+'use client';
+import {
+    FacebookShareButton,
+    TwitterShareButton,
+    WhatsappShareButton,
+    EmailShareButton,
+    FacebookIcon,
+    TwitterIcon,
+    WhatsappIcon,
+    EmailIcon
+} from 'react-share'
+
 
 const ShareButtons = ({ property }) => {
+    const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`;
+
     return (
         <button
             className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
