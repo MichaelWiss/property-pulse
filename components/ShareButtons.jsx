f1a75e9cd16155c1a0e41dd3bc36db1647fb5c83
MichaelWiss <michael.wiss@gmail.com>
2024-09-28 17:19:10 -0500

    twitter #

--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -18,9 +18,20 @@ const ShareButtons = ({ property }) => {
         <>
             <h3 className="text-xl font-bold text-center pt-2">Share This Property:</h3>
             <div className="flex gap-3 justify-center pb-5">
-                <FacebookShareButton url={shareUrl} quote={property.name} hashtag={`#${property.type.replace(/\s/g, '')}ForRent`}>
+                <FacebookShareButton 
+                    url={shareUrl} 
+                    quote={property.name} 
+                    hashtag={`#${property.type.replace(/\s/g, '')}ForRent`}
+                    >
                     <FacebookIcon size={40} round={true} />
                 </FacebookShareButton>
+                <TwitterShareButton 
+                    url={shareUrl} 
+                    title={property.name} 
+                    hashtags={[`${property.type.replace(/\s/g, '')}ForRent`]}
+                    >
+                    <TwitterIcon size={40} round={true} />
+                </TwitterShareButton>
             </div>
         </>
     );
