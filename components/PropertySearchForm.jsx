10705214ee7fc972e865556712af80b5e3d8b691
MichaelWiss <michael.wiss@gmail.com>
2024-09-30 13:28:35 -0500

    PropertySearchForm

--- components/PropertySearchForm.jsx
+++ components/PropertySearchForm.jsx
@@ -1,6 +1,27 @@
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 const PropertySearchForm = () => {
+    const [location, setLocation ]= useState('');
+    const [propertyType, setpropertyType ]= useState('All');
+
+    const router = useRouter();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (location === '' && propertyType === 'All') {
+            router.push('/properties');
+        } else {
+            const query = `?location=${location}&propertyType=${propertyType}`;
+            router.push(`/properties/search-results${query}`);
+        }
+    };
+
+
     return ( 
-        <form
+        <form onSubmit={ handleSubmit }
         className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center"
       >
         <div className="w-full md:w-3/5 md:pr-2 mb-4 md:mb-0">
@@ -10,6 +31,8 @@ const PropertySearchForm = () => {
             id="location"
             placeholder="Enter Location (City, State, Zip, etc"
             className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
           />
         </div>
         <div className="w-full md:w-2/5 md:pl-2">
@@ -17,6 +40,8 @@ const PropertySearchForm = () => {
           <select
             id="property-type"
             className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
+            value={propertyType}
+            onChange={(e) => setpropertyType(e.target.value)}
           >
             <option value="All">All</option>
             <option value="Apartment">Apartment</option>
