96771b84cbfeb4a3ccb9c8d00a65786bba438fb9
MichaelWiss <michael.wiss@gmail.com>
2024-10-03 18:57:12 -0500

    search results matching

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,5 +1,20 @@
-const SearchResultsPage = ({ searchParams: { location, propertyType } }) => {
-    console.log(location, propertyType);
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { convertToSerializableObject } from "@/utils/convertToObject";
+
+const SearchResultsPage = async ({ searchParams: { location, propertyType } }) => {
+    await connectDB();
+
+    const locationPattern = new RegExp(location, 'i');
+
+    let query = {
+        $or: [
+            {name: locationPattern},
+            {description: locationPattern},
+            {location.street: locationPattern},
+        ]
+    }
+
     return <div>Search Results</div>
 };
  
