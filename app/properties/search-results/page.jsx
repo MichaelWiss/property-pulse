5655d30e5b3469755a5489af6f256f3644738911
MichaelWiss <michael.wiss@gmail.com>
2024-10-14 17:57:41 -0500

    Search results

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -38,7 +38,19 @@ const SearchResultsPage = async ({ searchParams: { location, propertyType } }) =
             </div>
         </section>
         <section className="px -4 py-6">
-            .container
+            <div className="container-xl lg:container m-auto px-4 py-6">
+                <Link href="/properties" className='flex items-center text-blue-500 hover:underline mb-3'>
+                    <FaArrowAltCircleLeft className='mr-2 mb-1'/>Back to Properties
+                </Link>
+                <h1 className="text-2xl mb-4">Search Results</h1>
+                { properties.length === 0 ? (<p>No Search Results</p>) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {properties.map((property) => (
+                            <PropertyCard key={property._id} property={property} />
+                        )) }
+                    </div>
+                ) }
+            </div>
         </section>
     </>
 };
