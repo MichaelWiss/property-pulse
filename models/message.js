e01c98d656cb00dc9aa0e34f907e71a083df31b4
MichaelWiss <michael.wiss@gmail.com>
2024-10-16 16:53:19 -0500

    message schema

--- models/message.js
+++ models/message.js
@@ -1,28 +1,21 @@
 import { Schema, model, models } from 'mongoose';
 
-const UserSchema = new Schema({
-    email: {
-        type: String,
-        unique: [true, 'Email already exists'],
-        required: [true, 'Email is requires']
-    },
-    username: {
-        type: String,
-        required: [true, 'Username is required']
-    },
-    image: {
-        type: String
-    },
-    bookmarks: [
-        {
-        type: Schema.Types.ObjectId,
-        ref: 'Property'
-    }
-    ]
-}, {
+const UserSchema = new Schema(
+    {
+        sender: {
+            type: Schema.Types.ObjectId,
+            required: true
+        },
+        recipients: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+    }, 
+  {
     timestamps: true
 });
 
-const User = models.User || model('User', UserSchema);
+const Message = models.Message || model('Message', MessageSchema);
 
 export default User;
