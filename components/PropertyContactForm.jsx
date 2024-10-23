9bae38970c345704f622b66b0944bfbe640350b1
MichaelWiss <michael.wiss@gmail.com>
2024-10-23 18:59:28 -0500

    recipient message state

--- components/PropertyContactForm.jsx
+++ components/PropertyContactForm.jsx
@@ -12,13 +12,30 @@ import { FaPaperPlane } from "react-icons/fa";
 const PropertyContactForm = ({ property }) => {
     const { data: session } = useSession();
 
-
+    const {state, formAction} = useFormStatus(addMessage, {});
+    if(state.submitted) {
+      return (
+        <p className="text-green-500 mb-4">Your message has been sent</p>
+      );
+    }
 
     return (
       session && (
       <div className="bg-white p-6 rounded-lg shadow-md">
     <h3 className="text-xl font-bold mb-6">Contact Property Manager</h3>
     <form>
+    <input 
+        type="hidden" 
+        id="property" 
+        name="property" 
+        defaultValue={property._id}
+      />
+       <input 
+        type="hidden" 
+        id="recipient" 
+        name="recipient" 
+        defaultValue={property._owner}
+      />
     <div className='mb-4'>
       <label
         className='block text-gray-700 text-sm font-bold mb-2'
