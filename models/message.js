baf7c39e5d7f9c09bb38f46e4b9e731d98b2b830
MichaelWiss <michael.wiss@gmail.com>
2024-10-31 10:42:00 -0500

    validation error

--- models/message.js
+++ models/message.js
@@ -5,35 +5,39 @@ const MessageSchema = new Schema(
         sender: {
             type: Schema.Types.ObjectId,
             ref: 'User',
-            required: true
+            required: true,
         },
-        recipients: {
+        recipient: {
             type: Schema.Types.ObjectId,
             ref: 'User',
-            required: true
+            required: true,
         },
         property: {
             type: Schema.Types.ObjectId,
             ref: 'Property',
-            required: true
+            required: true,
         },
         name: {
             type: String,
-            required: [true, 'Name is required']
+            required: [true, 'Name is required'],
         },
         email: {
             type: String,
-            required: [true, 'Email is required']
+            required: [true, 'Email is required'],
         },
-        phone: String,
-        body: String,
+        phone: {
+            type: String,
+        },
+        body: {
+            type: String,
+        },
         read: {
             type: Boolean,
-            default: false
-        }
+            default: false,
+        },
     }, 
   {
-    timestamps: true
+    timestamps: true,
     }
 );
 
