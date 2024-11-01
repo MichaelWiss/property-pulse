8957925ffec1f8de317f12f52914c574093a00f0
MichaelWiss <michael.wiss@gmail.com>
2024-11-01 16:25:35 -0500

    SubmitMessageButton

---
+++ components/SubmitMessageButton.jsx
@@ -0,0 +1,14 @@
+import { FaPaperPlane } from 'react-icons/fa';
+import React from 'react'
+
+export default function SubmitMessageButton() {
+  return (
+    <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center"
+          type="submit"
+        >
+          <FaPaperPlane className="mr-2"/> Send Message
+        </button>
+  );
+};
+
