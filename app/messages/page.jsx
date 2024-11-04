c8ae6b24a2bd032bae762f560a09a1dd686a6478
MichaelWiss <michael.wiss@gmail.com>
2024-11-04 16:13:04 -0600

    messages

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import connectDB from '@/config/database';
 import Message from '@/models/message';
+import '@/models/Property';
+import { convertToSerializableObject } from '@/utils/convertToObject';
+import { getSessionUser } from '@/utils/getSessionUser';
 
 export default function MessagesPage() {
+    connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    const { userId } = sessionUser;
+
+    const readMessages  = await Message.find({ recipient: userId, read: true })
+
+
   return (
     <div>MessagesPage</div>
   )
