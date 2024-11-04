d4d644c2309bc2f2acd7d1073a74171efa3ff616
MichaelWiss <michael.wiss@gmail.com>
2024-11-04 15:49:40 -0600

    Messages Page

---
+++ app/messages/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import connectDB from '@/config/database';
+import Message from '@/models/message';
+
+export default function MessagesPage() {
+  return (
+    <div>MessagesPage</div>
+  )
+}
