26ebe9977714a32c2bad4c6ba8faa35f60990461
MichaelWiss <michael.wiss@gmail.com>
2024-11-04 17:50:46 -0600

    no messages

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -5,17 +5,54 @@ import '@/models/Property';
 import { convertToSerializableObject } from '@/utils/convertToObject';
 import { getSessionUser } from '@/utils/getSessionUser';
 
-export default function MessagesPage() {
+const MessagesPage = async () => {
     connectDB();
 
     const sessionUser = await getSessionUser();
 
     const { userId } = sessionUser;
 
-    const readMessages  = await Message.find({ recipient: userId, read: true })
+    const readMessages  = await Message.find({ recipient: userId, 
+        read: true, })
+            .sort({ createdAt: -1 })
+            .populate('sender', 'username')
+            .populate('property', 'name')
+            .lean();
+            
+     const unreadMessages  = await Message.find({ recipient: userId, 
+        read: false,})
+             .sort({ createdAt: -1 })
+             .populate('sender', 'username')
+             .populate('property', 'name')
+             .lean();
+
+        const messages = [...unreadMessages, ...readMessages].map((messageDoc) => {
+            const message = convertToSerializableObject(messageDoc);
+            message.sender = convertToSerializableObject(messageDoc.sender);
+            message.property = convertToSerializableObject(messageDoc.property);
+            return message;
+        });
 
 
   return (
-    <div>MessagesPage</div>
-  )
-}
+    <section className='bg-blue-50'>
+        <div className="container m-auto py-24 max-4-6xl">
+            <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+                <h1 className="text-3xl font-bold mb-4">Your Messages</h1>
+
+                <div className="space-y-4">
+                    {
+                        messages.length === 0 ? (<p>You have no messages</p>) : (
+                            messages.map((message) => {
+                                <h3 key={message._id}>{message.name}</h3>
+                            })
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    </section>  
+  );
+};
+
+export default MessagesPage;
