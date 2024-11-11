589e61555d60466081106c34715b9cb8c820d4f7
MichaelWiss <michael.wiss@gmail.com>
2024-11-11 16:28:12 -0600

    MessageCard accessing message object

--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -1,5 +1,10 @@
 const MessageCard = ({ message }) => {
-    return <div>{ message.name }</div>;
+    return <div className='relative bg-white p-4 rounded-md shadow-md border border-gray-200'>
+        <h2 className="text-xl mb-4">
+            <span className="font-bold">Property Inquiry:</span>{ ' ' }
+            {message.property.name}
+        </h2>
+    </div>;
 }
  
 export default MessageCard;
